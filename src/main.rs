3fdbda3d012be55c2f07dc69a14131cd456809c8
Evangelos Spyropoulos <evangelosspyrop@hotmail.com>
2021-10-06 16:52:41 +0300

    Add ground plane and torus

--- src/main.rs
+++ src/main.rs
@@ -22,11 +22,30 @@ fn setup(
     mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
     commands.spawn_bundle(PbrBundle {
+        mesh: meshes.add(Mesh::from(shape::Plane{size: 30.0})),
+        material: materials.add(Color::GRAY.into()),
+        ..Default::default()
+    });
+
+    commands.spawn_bundle(PbrBundle {
+        transform: Transform::from_xyz(0.0, 0.5, 0.0),
         mesh: meshes.add(Mesh::from(shape::Cube{size: 1.0})),
         material: materials.add(Color::rgb(0.0, 0.7, 0.8).into()),
         ..Default::default()
     })
     .insert(Rotator);
+    
+    commands.spawn_bundle(PbrBundle {
+        transform: Transform::from_xyz(3.0, 1.0, -4.0),
+        mesh: meshes.add(Mesh::from(shape::Torus {
+            radius: 1.0,
+            ring_radius: 0.5,
+            subdivisions_segments: 10,
+            subdivisions_sides: 5
+        })),
+        material: materials.add(Color::SALMON.into()),
+        ..Default::default()
+    });
     
     commands.spawn_bundle(LightBundle {
         transform: Transform::from_xyz(5.0, 4.0, 2.0),
@@ -34,7 +53,7 @@ fn setup(
     });
     
     commands.spawn_bundle(PerspectiveCameraBundle {
-        transform: Transform::from_xyz(-3.0, 2.5, 4.0).looking_at(Vec3::ZERO, Vec3::Y),
+        transform: Transform::from_xyz(0.0, 6.0, 10.0).looking_at(Vec3::ZERO, Vec3::Y),
         ..Default::default()
     })
     .insert(Camera);
@@ -45,7 +64,7 @@ fn rotation(
     mut query: Query<&mut Transform, With<Rotator>>
 ) {
     for mut transform in query.iter_mut() {
-        transform.rotate(Quat::from_rotation_z(1.0 * time.delta_seconds()));
+        transform.rotate(Quat::from_rotation_y(1.0 * time.delta_seconds()));
     }
 }
 
